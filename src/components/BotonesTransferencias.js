54d083de798331e0b2868d01b7f31ece9094a050
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-09-21 09:25:28 +0200

    cambios de react-development

--- src/components/BotonesTransferencias.js
+++ src/components/BotonesTransferencias.js
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import { FormularioTransferencia } from "./FormularioTransferencia";
 import { BiArrowToBottom, BiArrowToTop } from "react-icons/bi";
 
